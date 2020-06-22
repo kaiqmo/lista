ae015c182065a41353bc86d2a2501ffabf5efe14
kaiqmo <kaiqmo@gmail.com>
2020-06-22 08:17:10 -0300

    Updates

---
+++ precache-manifest.0731ae9ad163f0e86f2fd84b3828a635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c024c4fe5cbb5f9ce07843e302a05c88",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "994b2dc2252121260b3b",
+    "url": "/todoList/static/css/main.3edeccb4.chunk.css"
+  },
+  {
+    "revision": "4bb68a17c0cae302822c",
+    "url": "/todoList/static/js/2.9f764afb.chunk.js"
+  },
+  {
+    "revision": "ac00860f10b301ce4ff76aaa802ce348",
+    "url": "/todoList/static/js/2.9f764afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994b2dc2252121260b3b",
+    "url": "/todoList/static/js/main.d72f66ad.chunk.js"
+  },
+  {
+    "revision": "f8e2a15cfb8d93452ec0",
+    "url": "/todoList/static/js/runtime-main.4355a649.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/todoList/static/media/crown.ed28accb.svg"
+  }
+]);
