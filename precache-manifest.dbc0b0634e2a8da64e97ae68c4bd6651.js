674a45a12cb083b51878ab671c4c344a409f4e6e
kaiqmo <kaiqmo@gmail.com>
2020-07-12 14:00:20 -0300

    Updates

---
+++ precache-manifest.dbc0b0634e2a8da64e97ae68c4bd6651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dbeaa489d5a8477ae28fd8fd65b900",
+    "url": "//kaiqmo.github.io/lista/index.html"
+  },
+  {
+    "revision": "8af7d26a1bdda01a3eb5",
+    "url": "//kaiqmo.github.io/lista/static/css/main.f9ba1b76.chunk.css"
+  },
+  {
+    "revision": "e905e913244789ad450b",
+    "url": "//kaiqmo.github.io/lista/static/js/2.7359b59a.chunk.js"
+  },
+  {
+    "revision": "ac00860f10b301ce4ff76aaa802ce348",
+    "url": "//kaiqmo.github.io/lista/static/js/2.7359b59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af7d26a1bdda01a3eb5",
+    "url": "//kaiqmo.github.io/lista/static/js/main.0f27d75c.chunk.js"
+  },
+  {
+    "revision": "5da49ffb23bb71499ee5",
+    "url": "//kaiqmo.github.io/lista/static/js/runtime-main.7660eb7a.js"
+  },
+  {
+    "revision": "b03bf9ce10cd42cadbec282522d91fdd",
+    "url": "//kaiqmo.github.io/lista/static/media/logo.b03bf9ce.jpeg"
+  },
+  {
+    "revision": "2db977737f53da4f15f1e97d026a9cca",
+    "url": "//kaiqmo.github.io/lista/static/media/logo2.2db97773.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a23726191507f1fb1c8d97daa61728e3.js"
+  "//kaiqmo.github.io/lista/precache-manifest.dbc0b0634e2a8da64e97ae68c4bd6651.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//kaiqmo.github.io/lista/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
